1f2b28a1b63872885e0ee6088b49454c45459f8c
Mruduladevi245 <mruduladevi026@gmail.com>
2026-05-09 18:09:54 +0530

    Update DataBaseConnection.java

--- DataBaseConnection.java
+++ DataBaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DataBaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/fakenews_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Kolkata";
     private static final String USER = "root";
-    private static final String PASSWORD = "";  // ← empty password
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
